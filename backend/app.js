e68df02d13dd9b99ea9d1a24a0b040eceb401cdb
¨lautaro <shortlauty35@gmail.com>
2025-10-16 18:53:14 -0300

    se añadieron todos los datos backend otra vez, modificados para crear el modelo en la base de datos

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import { connectDB } from "./src/config/database.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { Router } from "./src/routes/index.js";
+//import { routes } from "./src/routes/index.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", Router);
+
+// app.use("/api", routes)
+
+app.listen(PORT, async () => {
+  try {
+    connectDB();
+    console.log(`Servidor encendido y corriendo en https://localhost:${PORT}`);
+  } catch (error) {
+    console.log("Error al encender el servidor" + error);
+  }
+});
---
+++ backend/src/models/usuario.model.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+export const UserModel = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("student", "administrator"),
+      allowNull: false,
+      defaultValue: "student",
+    },
+  },
+  {
+    paranoid: true,
+  }
+);
