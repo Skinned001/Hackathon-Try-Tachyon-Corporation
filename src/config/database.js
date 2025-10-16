360e4c52168f1677017a081f3c0aee123dc1e420
Laprida <psmathe001@gmail.com>
2025-10-16 15:15:07 -0300

    modelos basicos de estudiante y usuario creados, faltan las relaciones

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,6 @@
 import { Sequelize } from 'sequelize';
-import "dotenv/config";
-
-
-
+import dotenv from "dotenv";
+dotenv.config();
 
 export const sequelize = new Sequelize(
     process.env.DB_NAME,
